a8cb430593f56fa2e1cda7418fd5ef876fa293e6
ZhongQing <richard_johnson@sina.com>
2018-01-06 15:40:57 +0800

    display user's current starred repo

--- HelloWorld/view.py
+++ HelloWorld/view.py
@@ -8,6 +8,10 @@ def hello(request):
     context['hello'] = 'Hello World!'
     context['woo'] ='you bad boy'
     gh = github.GitHub()
-    issues = gh.repos('michaelliao')('githubpy').issues.get()
-    context['issues'] = issues
+    #issues = gh.repos('michaelliao')('githubpy').issues.get()
+    repos= gh.users('richardissuperman').starred.get()
+    #for repo in repos:
+    #    print(repo['url'])
+    #context['issues'] = issues
+    context['repos'] = repos
     return render(request, 'hello.html', context)
