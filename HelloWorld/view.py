a22e812bb27e661e022837b92de465d56e61b84a
ZhongQing <richard_johnson@sina.com>
2018-01-06 22:49:14 +0800

    use authenticated user object as login

--- HelloWorld/view.py
+++ HelloWorld/view.py
@@ -7,11 +7,8 @@ def hello(request):
     context          = {}
     context['hello'] = 'Hello World!'
     context['woo'] ='you bad boy'
-    gh = github.GitHub()
-    #issues = gh.repos('michaelliao')('githubpy').issues.get()
-    repos= gh.users('richardissuperman').starred.get()
-    #for repo in repos:
-    #    print(repo['url'])
-    #context['issues'] = issues
+    gh = github.GitHub(username='usrname', password='password')
+    repos = gh.user().starred.get()
+#    repos= gh.users('richardissuperman').starred.get()
     context['repos'] = repos
     return render(request, 'hello.html', context)
