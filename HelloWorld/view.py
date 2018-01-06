941a3028b4239a62a650598778f306f65d983755
ZhongQing <richard_johnson@sina.com>
2018-01-06 15:22:21 +0800

    make templete work, test github issues list

--- HelloWorld/view.py
+++ HelloWorld/view.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . import github
+
 
 def hello(request):
     context          = {}
     context['hello'] = 'Hello World!'
     context['woo'] ='you bad boy'
+    gh = github.GitHub()
+    issues = gh.repos('michaelliao')('githubpy').issues.get()
+    context['issues'] = issues
     return render(request, 'hello.html', context)
